bff768331715701bd9498b76828f0a22391733eb
Sahil Bhutani <sahilbhutani2002@gmail.com>
2025-04-08 19:21:54 +0530

    update changes

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('students/',views.StudentsView.as_view()),
-    path('students/<int:pk>',views.StudentsIDView.as_view()),
+    path('students/<int:pk>/',views.StudentsIDView.as_view()),
 ]
 
